2186a0b359933c3112d320613d9dfc1a8405cf69
zhousongsong <zhousongsong@baidu.com>
2015-12-21 14:33:52 +0800

    add util file for common

---
+++ util.cpp
@@ -0,0 +1,22 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2015 Baidu.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ /**
+ * @file common.cpp
+ * @author zhousongsong(zhousongsong@baidu.com)
+ * @date 2015/12/21 14:31:24
+ * @brief 
+ *  
+ **/
+#include <stdio.h>
+#include "brocbuild/common/util.h"
+
+void help()
+{
+    printf("%s", "broc help");
+}
+/* vim: set ts=4 sw=4 sts=4 tw=100 */
---
+++ util.h
@@ -0,0 +1,20 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2015 Baidu.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ /**
+ * @file commn.h
+ * @author zhousongsong(zhousongsong@baidu.com)
+ * @date 2015/12/21 14:30:51
+ * @brief 
+ *  
+ **/
+#ifndef COMMN_H
+#define COMMN_H
+void help();
+#endif  // COMMN_H
+
+/* vim: set ts=4 sw=4 sts=4 tw=100 */
